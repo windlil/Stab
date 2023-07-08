9513518e57de9823af85111b91e5da67b2de9369
spacesouls <spacesouls@163.com>
2023-07-08 16:43:01 +0800

    1.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path, { resolve } from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
-    alias: {
-      "@/*": resolve(__dirname, 'src')
-    },
     extensions: ['.ts','.vue']
   }
 })
